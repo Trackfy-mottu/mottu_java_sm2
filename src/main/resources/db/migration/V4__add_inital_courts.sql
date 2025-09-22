34d879dba24b6e860d796c3a0334553b8ef57e37
guialvesped <alvespedrosog@gmail.com>
2025-09-21 22:30:19 -0300

    Add new migration to insert initial courts

---
+++ src/main/resources/db/migration/V4__add_inital_courts.sql
@@ -0,0 +1,7 @@
+INSERT INTO court
+(acess_code, name, address, max_capacity, current_bikes)
+VALUES
+    ('COURT_MORUMBI', 'Pátio Morumbi Shopping', 'Avenida Roque Petroni Júnior, 1089 - Morumbi, São Paulo - SP, 04707-900', 80, 0),
+    ('COURT_JARDINS', 'Pátio Jardins', 'Rua Oscar Freire, 379 - Jardins, São Paulo - SP, 01426-001', 35, 4),
+    ('COURT_LIBERDADE', 'Pátio Liberdade', 'Rua Galvão Bueno, 209 - Liberdade, São Paulo - SP, 01506-000', 40, 1),
+    ('COURT_BROOKLIN', 'Pátio Brooklin Corporate', 'Avenida Engenheiro Luís Carlos Berrini, 1140 - Brooklin, São Paulo - SP, 04571-000', 65, 1);
